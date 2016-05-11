2afa3f4265ba06dd939df114990d9ed9e3d8311a
ricardo <ricardo@ricardo-pc>
2016-05-11 11:55:03 -0430

    primer commit

---
+++ tarea3/BilleteraElectronica.py
@@ -0,0 +1,17 @@
+'''
+Created on May 11, 2016
+
+@author: ricardo
+'''
+
+class BilleteraElectronica:
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ tarea3/test_BilleteraElectronica.py
@@ -0,0 +1,17 @@
+'''
+Created on May 11, 2016
+
+@author: ricardo
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
