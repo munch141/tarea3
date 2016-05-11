4e8d25ecd20c15e6174bc6abdcaea257992a63ce
ricardo <ricardo@ricardo-pc>
2016-05-11 12:29:54 -0430

    se agregó prueba de agregarTransaccion

--- tarea3/BilleteraElectronica.py
+++ tarea3/BilleteraElectronica.py
@@ -3,15 +3,25 @@ Created on May 11, 2016
 
 @author: ricardo
 '''
+class Transaccion:
+    def __init__(self, monto, fecha, id_rest):
+        pass
+        
+class Historial:
+    def __init__(self):
+        pass
+        
+    def agregarTransaccion(self, t):
+        pass
 
 class BilleteraElectronica:
     '''
     classdocs
     '''
 
-
     def __init__(self, params):
         '''
         Constructor
         '''
+        pass
         
--- tarea3/test_BilleteraElectronica.py
+++ tarea3/test_BilleteraElectronica.py
@@ -4,12 +4,23 @@ Created on May 11, 2016
 @author: ricardo
 '''
 import unittest
+from datetime import *
+from BilleteraElectronica import *
 
 
 class Test(unittest.TestCase):
 
-    def testName(self):
-        pass
+    def testAgregarTransaccion(self):
+        t = Transaccion(1000, datetime(2016, 5, 11, 12, 0), 1)
+        h = Historial()
+        h.agregarTransaccion(t)
+        
+        self.assertEquals(len(h.transacciones), 1)
+        self.assertEquals(h.transacciones[len(h.transacciones)-1].monto, 1000)
+        self.assertEquals(h.transacciones[len(h.transacciones)-1].fecha,
+                          datetime(2016, 5, 11, 12, 0))
+        self.assertEquals(h.transacciones[len(h.transacciones)-1].id_rest, 1)
+        self.assertEquals(h.total, 1000)
 
 
 if __name__ == "__main__":
